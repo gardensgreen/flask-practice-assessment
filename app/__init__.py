caefac7cd8ac77bda126e54a0b40569bc4134556
Daniel TIllero <danieltillero67@gmail.com>
2020-12-13 13:16:22 -0500

    Post simple-form

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_migrate import Migrate
 from .forms import SimpleForm
 from .config import Config
+from .models import db, SimplePerson
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
@@ -16,3 +20,16 @@ def main_page():
 def simple_form():
     form = SimpleForm()
     return render_template("simple_form.html", form=form)
+
+
+@app.route("/simple-form", methods=["POST"])
+def simple_form_post():
+    form = SimpleForm()
+    if form.validate_on_submit():
+        person = SimplePerson()
+        form.populate_obj(person)
+        db.session.add(person)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return "Bad Data"
